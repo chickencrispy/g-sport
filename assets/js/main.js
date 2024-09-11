74ce441a7dd372f4ee92362dc7d9b910c0dc3fff
chickencrispy <debi.rahmadi@gmail.com>
2024-09-11 19:56:06 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const mainHeroSwiper = new Swiper(".swiper-main-hero", {
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+const reservationSwiper = new Swiper(".swiper-booking", {
+  slidesPerView: 1.7,
+  spaceBetween: 16,
+});
